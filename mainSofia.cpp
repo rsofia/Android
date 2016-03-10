4434cf23f496108eff2e6a14ba35e38ec6250ac8
rsofia <ruthsofia95@hotmail.com>
2016-03-10 12:20:12 -0600

    Android
    Primer programa de ejemplo

---
+++ mainSofia.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+int main()
+{
+	cout << "Hello world\n";
+
+	system("pause");
+	return 0;
+}
